c253654895adb4ac36035f7ef294c86cd01c4309
daoyeyemi <67729483+daoyeyemi@users.noreply.github.com>
2021-07-05 15:28:53 -0700

    added a bunch of form inputs and basic template for rest of the address form

--- src/components/CheckoutForm/AddressForm.js
+++ src/components/CheckoutForm/AddressForm.js
@@ -14,7 +14,38 @@ const AddressForm = () => {
                 <form onSubmit="">
                     <Grid container spacing={3}>
                         <FormInput required name="firstName" label="First name" />
+                        <FormInput required name="lastName" label="Last name" />
+                        <FormInput required name="address1" label="Address line 1" />
+                        <FormInput required name="email" label="Email" />
+                        <FormInput required name="city" label="City" />
+                        <FormInput required name="zip" label="Zip / Postal code" />
+                        <Grid item xs={12} sm={6}>
+                            <InputLabel>Shipping Country</InputLabel>
+                            <Select>
+                                <MenuItem>
+                                </MenuItem>
+                            </Select>
+                        </Grid>
+                        <Grid item xs={12} sm={6}>
+                            <InputLabel>Shipping Subdivision</InputLabel>
+                            <Select>
+                                <MenuItem>
+                                </MenuItem>
+                            </Select>
+                        </Grid>
+                        <Grid item xs={12} sm={6}>
+                            <InputLabel>Shipping Options</InputLabel>
+                            <Select>
+                                <MenuItem>
+                                </MenuItem>
+                            </Select>
+                        </Grid>
                     </Grid>
+                    <br></br>
+                    <div>
+                        <Button></Button>
+                        <Button></Button>
+                    </div>
                 </form>
             </FormProvider>
         </>
