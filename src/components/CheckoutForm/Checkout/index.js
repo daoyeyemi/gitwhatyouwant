b60331cb6cbb0409a27133f246e0113282045f7c
daoyeyemi <67729483+daoyeyemi@users.noreply.github.com>
2021-07-05 01:26:15 -0700

    added some useful hooks and made some useful objects accessible

--- src/components/CheckoutForm/Checkout/index.js
+++ src/components/CheckoutForm/Checkout/index.js
@@ -1,10 +1,31 @@
+import classes from '*.module.css'
+import { Paper, Stepper, Step, StepLabel, Typography, CircularProgress, Divider, Button } from '@material-ui/icons'
 import React from 'react'
 
 const Checkout = () => {
+    const [checkoutToken, setCheckoutToken] = useState(null);
+    const [activeStep, setActiveStep] = useState(0);
+    const [shippingData, setShippingData] = useState({});
+    const classes = useStyles();
+    const history = useHistory();
+    
     return (
-        <div>
-            
-        </div>
+        <>
+            <div className={classes.toolbar} />
+                <main className={classes.layout}>
+                    <Paper className={classes.paper}>
+                        <Typography variant="h4" align="center">Checkout</Typography>
+                        <Stepper className={classes.stepper}>
+                            {steps.map((label) => (
+                                <Step key={label}>
+                                    <StepLabel>{label}</StepLabel>
+                                </Step>
+                            ))}
+                        </Stepper>
+                    </Paper>
+                </main>
+        </>
+        
     )
 }
 
